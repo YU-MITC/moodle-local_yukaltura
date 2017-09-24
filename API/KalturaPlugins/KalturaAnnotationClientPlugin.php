30b1fa86440e50fe00f1719a1829976047d000f7
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2017-09-25 04:48:19 +0900

    Add files via upload

--- API/KalturaPlugins/KalturaAnnotationClientPlugin.php
+++ API/KalturaPlugins/KalturaAnnotationClientPlugin.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
+require_once(dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/config.php');
 require_once(dirname(__FILE__) . "/../KalturaClientBase.php");
 require_once(dirname(__FILE__) . "/../KalturaEnums.php");
 require_once(dirname(__FILE__) . "/../KalturaTypes.php");
