4d367c8f435de45f63f1fda8ab267d8e34d3bbaa
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2017-09-25 03:22:26 +0900

    Add files via upload

--- API/KalturaClient.php
+++ API/KalturaClient.php
@@ -22,6 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
 require_once("KalturaClientBase.php");
 require_once("KalturaEnums.php");
 require_once("KalturaTypes.php");
--- API/KalturaClientBase.php
+++ API/KalturaClientBase.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
+
 require_login();
 
 /**
