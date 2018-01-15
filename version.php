29aa5916fca0ada7f01f36b5cd96744cd2cb4c34
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2018-01-15 20:19:09 +0900

    Add files via upload

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018011100;
+$plugin->version = 2018011500;
 $plugin->component = 'local_yukaltura';
 $plugin->release = 'YU Kaltura Media Local Libraries 1.1.2';
 $plugin->requires = 2015051100;
