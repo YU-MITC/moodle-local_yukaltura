cc60c6734321ae6a9c7069eef176fa5cea3b3b14
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2023-01-20 14:14:59 +0900

    Update

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_yukaltura';
-$plugin->version = 2023011900;
+$plugin->version = 2023012000;
 $plugin->release = '2.1.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2022041900;
